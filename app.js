bdd2a73c00f44a7b78ff2401ca200d3b9b714f69
Kaleb Kuo <klkuo.cx@gmail.com>
2020-09-24 08:32:17 +0800

    feat:add server log

---
+++ app.js
@@ -0,0 +1,52 @@
+// Require express and server related variables
+const express = require('express')
+const app = express()
+const port = 3000
+
+// Define log functions
+function reqLog(req, res, next) {
+  console.log(`${timeStamps()} | ${req.method} from ${req.originalUrl}`)
+  next()
+}
+
+function timeStamps() {
+  const timeNow = new Date()
+  const year = timeNow.getFullYear()
+  const month = timeNow.getMonth()
+  const date = timeNow.getDate()
+  const hour = timeNow.getHours()
+  const min = timeNow.getMinutes()
+  const sec = timeNow.getSeconds()
+  const timeStamps = `${year}-${month}-${date} ${hour}:${min}:${sec}`
+  return timeStamps
+}
+
+// Set middleware as server log
+app.use((req, res, next) => {
+  const { id } = req.params
+  if (id !== 'favicon.ico') {
+    reqLog(req, res, next)
+  }
+})
+
+// Set routes
+app.get('/', (req, res) => {
+  res.send('列出全部 Todo')
+})
+
+app.get('/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+app.get('/:id', (req, res) => {
+  res.send('顯示一筆 Todo')
+})
+
+app.post('/', (req, res) => {
+  res.send('新增一筆  Todo')
+})
+
+// Listen to server
+app.listen(port, () => {
+  console.log(`App running on port http://localhost:${port}`)
+})
